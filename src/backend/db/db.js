2375a21d6b7dadb1d216278f58ce9ab0c08231c3
william <williamap@gmail.com>
2025-07-15 22:05:04 -0400

    Add what's necessary to get docker fully working to host the application online

--- src/backend/db/db.js
+++ src/backend/db/db.js
@@ -1,7 +1,7 @@
 import postgres from "postgres";
 
 const sql = postgres({
-  host: "localhost", // Postgres ip address[s] or domain name[s]
+  host: "db", // Postgres ip address[s] or domain name[s]
   port: 5432, // Postgres server port[s]
   database: "postgres", // Name of database to connect to
   username: "postgres", // Username of database user
